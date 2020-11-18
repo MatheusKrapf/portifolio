25bddc4081de60f6b8dad9f67cb67d5d0c71211b
leandro-M <leandrotinfo19@gmail.com>
2020-11-17 21:17:32 -0300

    setup react-testing library

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+﻿import React from 'react'
+
+// import { Container } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <main>
+      <h1>boilerplate</h1>
+    </main>
+  )
+}
+
+export default Main
---
+++ src/components/Main/test.tsx
@@ -0,0 +1,13 @@
+﻿import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /boilerplate/i })
+    ).toBeInTheDocument()
+  })
+})
